850d8b916aea518f1fb734f28314c2b429f4a1ac
Wenson Tsai <wensontsai@gmail.com>
2017-05-22 20:22:32 -0400

    adding blog

--- src/components/bizcard.jsx
+++ src/components/bizcard.jsx
@@ -5,50 +5,57 @@ var Reflux = require('reflux');
 var Actions = require('../actions');
 
 module.exports = React.createClass({
-	mixins: [
-		// Reflux.listenTo(ImageStore, 'onChange')
-	],
-	getInitialState: function(){
-		return{
-			images: []
-		}
-	},
-	componentWillMount: function(){
-		// Actions.getImages(this.props.params.id);
-	},
-	render: function(){
-		return(
-			<div className="bizcard">
-				<div className="row content">
-					<div className="content">
-						<span className="name">Wenson Tsai</span>
-						<span className="info">Fullstack Software Engineer / Developer / Musician</span>
-					</div>
-					<div className="links">
-						<ul>
-							<li><a href="https://twitter.com/itswenson" target="blank"><i className="fa fa-twitter big-icon"></i></a></li>
-							<li><a href="https://github.com/wensontsai" target="blank"><i className="fa fa-github big-icon"></i></a></li>
-							<li><a href="https://linkedin.com/in/wensontsai" target="blank"><i className="fa fa-linkedin big-icon"></i></a></li>
-						</ul>
-					</div>
-				</div>
-				<div className="row story">
-					<div className="story">
-						Wenson Tsai is a Fullstack Software Engineer and Musician who spends his most precious moments creating.  Whether building excitement and experiences through code, product, technology, songwriting, or performance, the focus remains on next-level innovation and craftsmanship.  Previously, he worked in the music industry processing licenses and deals for all the major players: Spotify, Apple, Google, major music labels, et al. The software he wrote turned data into royalty checks for everyone whose music you have ever enjoyed.  Presently, he writes the best <a href="https://www.makerbot.com/print/" target="blank">desktop app</a> for 3D-printing dreams to life, while pushing model-slicing and toolpathing algorithms towards industry innovation.  In another life, he was a <a href="https://www.flickr.com/photos/itswenson/" target="blank">photographer</a> who traveled and exhibited internationally.
-					</div>
-				</div>
-			</div>
-		);	
-	},
-	renderImages: function(){
-		return this.state.images.map(function(image){
-			return <ImagePreview key={image.id} {...image} />
-		});
-	},
-	componentWillReceiveProps: function(nextProps){
-		// Actions.getImages(nextProps.params.id);
-	},
-	onChange: function(event, images){
-		this.setState( {images: images} )
-	}
+    mixins: [
+        // Reflux.listenTo(ImageStore, 'onChange')
+    ],
+    getInitialState: function(){
+        return{
+            images: []
+        }
+    },
+    componentWillMount: function(){
+        // Actions.getImages(this.props.params.id);
+    },
+    render: function(){
+        return(
+            <div className="bizcard">
+                <div className="row content">
+                    <div className="content">
+                        <span className="name">Wenson Tsai</span>
+                        <span className="info">Fullstack Software Engineer / Developer / Musician</span>
+                    </div>
+                    <div className="links">
+                        <ul>
+                            <li><a href="https://twitter.com/itswenson" target="blank"><i className="fa fa-twitter big-icon"></i></a></li>
+                            <li><a href="https://github.com/wensontsai" target="blank"><i className="fa fa-github big-icon"></i></a></li>
+                            <li><a href="https://linkedin.com/in/wensontsai" target="blank"><i className="fa fa-linkedin big-icon"></i></a></li>
+                            <li><a href="http://hackers.wenson.io" target="blank"><i className="fa fa-newspaper-o big-icon"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="row story">
+                    <div className="story">
+                        Wenson Tsai is a Fullstack Software Engineer and Musician who spends his most precious moments creating.  Whether building excitement and experiences through code, product, technology, songwriting, or performance, the focus remains on next-level innovation and craftsmanship.
+                        </br>
+                        Previously, he worked in the music industry processing licenses and deals for all the major players: Spotify, Apple, Google, major music labels, et al. The software he wrote turned data into royalty checks for everyone whose music you have ever enjoyed.  
+                        </br>
+                        Presently, he writes the <a href="https://www.makerbot.com/print/" target="blank">desktop app</a> for 3D-printing dreams to life, while pushing model-slicing and toolpathing algorithms towards industry innovation.  
+                        </br>
+                        In another life, he was a <a href="https://www.flickr.com/photos/itswenson/" target="blank">photographer</a> who traveled and exhibited internationally.
+                    </div>
+                </div>
+            </div>
+        );  
+    },
+    renderImages: function(){
+        return this.state.images.map(function(image){
+            return <ImagePreview key={image.id} {...image} />
+        });
+    },
+    componentWillReceiveProps: function(nextProps){
+        // Actions.getImages(nextProps.params.id);
+    },
+    onChange: function(event, images){
+        this.setState( {images: images} )
+    }
 });
